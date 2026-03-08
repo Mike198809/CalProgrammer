f6a5e8c6a37ee8ff59998f875df029a507e65703
Mike198809 <miguelbarreiro09@gmail.com>
2026-03-08 13:30:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,41 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("progcalc").then(cache=>{
-return cache.addAll([
-"./",
-"./index.html",
-"./manifest.json",
-"./icon.png"
-])
+const CACHE_NAME = "progcalc-v1";
+
+const FILES_TO_CACHE = [
+"/",
+"/index.html",
+"/manifest.json",
+"/icon.png",
+"/sw.js"
+];
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(FILES_TO_CACHE);
 })
-)
+);
+self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
 })
+);
+})
+);
+self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
+});
